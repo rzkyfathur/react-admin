d11400418c43847f3a1c23122bab758ab51290af
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-02-17 10:58:57 +0700

    create data files

---
+++ src/data.js
@@ -0,0 +1,131 @@
+import Avatar from "./avatar.jpg"
+
+export const userData = [{
+        name: 'Jan',
+        "Active User": 10000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 8000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 7000,
+    },
+    {
+        name: 'Apr',
+        "Active User": 8000,
+    },
+    {
+        name: 'Jun',
+        "Active User": 5000,
+    },
+    {
+        name: 'Jul',
+        "Active User": 500,
+    },
+    {
+        name: 'Aug',
+        "Active User": 9000,
+    },
+    {
+        name: 'Sept',
+        "Active User": 9000,
+    },
+    {
+        name: 'Oct',
+        "Active User": 9000,
+    },
+    {
+        name: 'Nov',
+        "Active User": 8000,
+    },
+    {
+        name: 'Des',
+        "Active User": 7000,
+    },
+];
+
+
+export const userRows = [{
+        id: 1,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 2,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 3,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 4,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 5,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 6,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 7,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 8,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 9,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+    {
+        id: 10,
+        username: "Cuo",
+        avatar: Avatar,
+        email: "Cuo@gmail.com",
+        status: "active",
+        transaction: "$120.00"
+    },
+
+];
