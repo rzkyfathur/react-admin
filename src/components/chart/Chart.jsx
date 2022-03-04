e19182eead033038cf1c03c2257a6718f187c537
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-03-04 10:17:39 +0700

    create chart jsx

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -12,7 +12,7 @@ export default function Chart({title, data,  grid}) {
             <ResponsiveContainer width="100%" aspect={4 / 1}>
                 <LineChart data={data}>
                     <XAxis dataKey="name" stroke="#5550bd"/>
-                    <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+                    <Line type="monotone" dataKey="Sales" stroke="#5550bd" />
                     <Tooltip/>
                     {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5"/>}
                 </LineChart>
