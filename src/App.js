f25598c0d6f35adfb46d2101857890be666df81f
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-03-06 10:03:04 +0700

    update and create file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Home from "./components/page/Home/Home";
 import Sidebar from "./components/sidebar/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./components/userList/UserList";
+import User from "./components/user/User";
+import CreateUser from "./components/page/createUser/CreateUser";
+import ProductList from "./components/page/productList/ProductList";
+import Product from "./components/page/product/Product";
+import CreateProduct from "./components/page/createProduct/CreateProduct";
 
 
 
@@ -18,6 +23,11 @@ function App() {
         <Switch>
           <Route exact path="/"><Home/></Route>
           <Route path="/users"><UserList/></Route>
+          <Route path="/user/:userId"><User/></Route>
+          <Route path="/createUser"><CreateUser/></Route>
+          <Route path="/products"><ProductList/></Route>
+          <Route path="/product/:productId"><Product/></Route>
+            <Route path="/createProduct"><CreateProduct/></Route>
         </Switch>
       
       </div>
